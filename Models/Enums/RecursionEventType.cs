5ee6f501454cceb04ddfca9a102894a7fc0b8c7f
Kirill Parakhin <kirill.parakhin@altenar.com>
2023-11-29 21:46:49 +0300

    Add Service Test projects with simple tests for creating, building and healt checking hosted service

--- Models/Enums/RecursionEventType.cs
+++ Models/Enums/RecursionEventType.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Models.Enums;
 
-namespace Models.Enums
+public enum RecursionEventType
 {
-    internal class RecursionEventType
-    {
-    }
+    None,
+
+    EveryDay,
+
+    EveryWeek,
+
+    EveryMonth
 }
