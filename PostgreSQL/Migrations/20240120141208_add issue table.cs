59585e0b5b20bfd7d1e3dae21b99614828db1319
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-01-20 17:15:22 +0300

    add issue migrations

---
+++ PostgreSQL/Migrations/20240120141208_add issue table.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Models.Enums;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addissuetable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:decision_type", "none,default,apply,deny")
+                .Annotation("Npgsql:Enum:event_status", "none,not_started,within_reminder_offset,live,finished,cancelled")
+                .Annotation("Npgsql:Enum:event_type", "none,personal,one_to_one,stand_up,meeting")
+                .Annotation("Npgsql:Enum:group_type", "none,educational,job")
+                .Annotation("Npgsql:Enum:issue_type", "none,bag_issue,violation_issue")
+                .Annotation("Npgsql:Enum:snapshot_type", "none,events_snapshot,tasks_snapshot,issues_snapshot,reports_snapshot")
+                .Annotation("Npgsql:Enum:task_current_status", "none,to_do,in_progress,review,done")
+                .Annotation("Npgsql:Enum:task_type", "none,abstract_goal,meeting_presense,job_complete")
+                .OldAnnotation("Npgsql:Enum:decision_type", "none,default,apply,deny")
+                .OldAnnotation("Npgsql:Enum:event_status", "none,not_started,within_reminder_offset,live,finished,cancelled")
+                .OldAnnotation("Npgsql:Enum:event_type", "none,personal,one_to_one,stand_up,meeting")
+                .OldAnnotation("Npgsql:Enum:group_type", "none,educational,job")
+                .OldAnnotation("Npgsql:Enum:snapshot_type", "none,events_snapshot,tasks_snapshot,issues_snapshot,reports_snapshot")
+                .OldAnnotation("Npgsql:Enum:task_current_status", "none,to_do,in_progress,review,done")
+                .OldAnnotation("Npgsql:Enum:task_type", "none,abstract_goal,meeting_presense,job_complete");
+
+            migrationBuilder.CreateTable(
+                name: "issues",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    issue_type = table.Column<IssueType>(type: "issue_type", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    img_link = table.Column<string>(type: "text", nullable: false),
+                    issue_moment = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_issues", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_issues_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_issues_user_id",
+                table: "issues",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "issues");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:decision_type", "none,default,apply,deny")
+                .Annotation("Npgsql:Enum:event_status", "none,not_started,within_reminder_offset,live,finished,cancelled")
+                .Annotation("Npgsql:Enum:event_type", "none,personal,one_to_one,stand_up,meeting")
+                .Annotation("Npgsql:Enum:group_type", "none,educational,job")
+                .Annotation("Npgsql:Enum:snapshot_type", "none,events_snapshot,tasks_snapshot,issues_snapshot,reports_snapshot")
+                .Annotation("Npgsql:Enum:task_current_status", "none,to_do,in_progress,review,done")
+                .Annotation("Npgsql:Enum:task_type", "none,abstract_goal,meeting_presense,job_complete")
+                .OldAnnotation("Npgsql:Enum:decision_type", "none,default,apply,deny")
+                .OldAnnotation("Npgsql:Enum:event_status", "none,not_started,within_reminder_offset,live,finished,cancelled")
+                .OldAnnotation("Npgsql:Enum:event_type", "none,personal,one_to_one,stand_up,meeting")
+                .OldAnnotation("Npgsql:Enum:group_type", "none,educational,job")
+                .OldAnnotation("Npgsql:Enum:issue_type", "none,bag_issue,violation_issue")
+                .OldAnnotation("Npgsql:Enum:snapshot_type", "none,events_snapshot,tasks_snapshot,issues_snapshot,reports_snapshot")
+                .OldAnnotation("Npgsql:Enum:task_current_status", "none,to_do,in_progress,review,done")
+                .OldAnnotation("Npgsql:Enum:task_type", "none,abstract_goal,meeting_presense,job_complete");
+        }
+    }
+}
