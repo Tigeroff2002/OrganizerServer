fe681657d8f54cd6066f1246a4a242a7a2201370
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-02-12 17:37:32 +0300

    add migration files

--- Database/Migrations/10_data_feed.sql
+++ Database/Migrations/10_data_feed.sql
@@ -1,13 +1,13 @@
-INSERT INTO groups (group_name, type) VALUES ('Add dot net pls', 'educational');
-INSERT INTO groups(group_name, type) VALUES ('ADF', 'job');
-INSERT INTO groups(group_name, type) VALUES ('pri120', 'educational');
-
 INSERT INTO users (user_name, role, email, password, phone_number, auth_token, account_creation) 
 VALUES ('Kirill', 'user', 'kirill.parakhin@altenar.com', 'kirill2002', '+79042555027', '0347837483', '2023-09-20 12:00:00.000 +0300');
 INSERT INTO users (user_name, role, email, password, phone_number, auth_token, account_creation) 
 VALUES ('Tigeroff', 'user', 'parahinkirill2002@yandex.ru', 'tigeroff2002', '+79032565027', '0745637489', '2023-10-19 12:00:00.000 +0300');
 INSERT INTO users (user_name, role, email, password, phone_number, auth_token, account_creation) 
 VALUES ('Nikita', 'user', 'portnovnikitos2002@yandex.ru', 'nikita2002', '+79302555027', '0890837483', '2023-12-20 12:00:00.000 +0300');
+
+INSERT INTO groups (group_name, type, manager_id) VALUES ('Add dot net pls', 'educational', 1);
+INSERT INTO groups (group_name, type, manager_id) VALUES ('ADF', 'job', 1);
+INSERT INTO groups (group_name, type, manager_id) VALUES ('pri120', 'educational', 1);
 
 INSERT INTO events 
 (caption, description, scheduled_start, duration, event_type, status, related_group_id, manager_id)
@@ -70,9 +70,16 @@ INSERT INTO tasks (caption, description, task_type, task_status, reporter_id, im
 VALUES ('December meeting presense', 'Be presense on december ADF common meetings', 
        'meeting_presense', 'in_progress', 1, 1);
 
-INSERT INTO snapshots (description, snapshot_type, begin_moment, end_moment, user_id)
-VALUES ('Empty snapshot about Kirill`s events in january', 'events_snapshot',
+INSERT INTO snapshots (description, snapshot_type, create_moment, begin_moment, end_moment, user_id)
+VALUES ('Empty snapshot about Kirill`s events in january', 'events_snapshot', '2024-02-10 10:00:00.000 +0300',
         '2024-01-01 12:00:00.000 +0300', '2024-01-30 00:00:00.000 +0300', 1);
-INSERT INTO snapshots (description, snapshot_type, begin_moment, end_moment, user_id)
-VALUES ('Empty snapshot about Nikits`s tasks in february beginning', 'tasks_snapshot',
+INSERT INTO snapshots (description, snapshot_type, create_moment, begin_moment, end_moment, user_id)
+VALUES ('Empty snapshot about Nikits`s tasks in february beginning', 'tasks_snapshot', '2024-02-20 10:00:00.000 +0300',
         '2024-02-01 14:00:00.000 +0300', '2024-02-10 10:00:00.000 +0300', 3);
+
+INSERT INTO issues (issue_type, title, description, img_link, issue_moment, user_id, status)
+VALUES ('bag_issue', 'Restart with component error', 'Retryable restart of backend app during 10 minutes',
+    'https://ir.ozone.ru/s3/multimedia-d/wc1000/6331992373.jpg', '2024-02-10 10:00:00.000 +0300', 1, 'in_progress');
+INSERT INTO issues (issue_type, title, description, img_link, issue_moment, user_id, status)
+VALUES ('violation_issue', 'Violation of user rules', 'User Tigeroff has breaked up some rules of license',
+    'https://ir.ozone.ru/s3/multimedia-m/wc1000/6400319302.jpg', '2024-02-11 10:00:00.000 +0300', 2, 'reported');
---
+++ PostgreSQL/Migrations/20240212142239_group manager id and issue status.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Models.Enums;
+
+#nullable disable
+
+namespace PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class groupmanageridandissuestatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:decision_type", "none,default,apply,deny")
+                .Annotation("Npgsql:Enum:event_status", "none,not_started,within_reminder_offset,live,finished,cancelled")
+                .Annotation("Npgsql:Enum:event_type", "none,personal,one_to_one,stand_up,meeting")
+                .Annotation("Npgsql:Enum:group_type", "none,educational,job")
+                .Annotation("Npgsql:Enum:issue_status", "none,reported,in_progress,closed")
+                .Annotation("Npgsql:Enum:issue_type", "none,bag_issue,violation_issue")
+                .Annotation("Npgsql:Enum:snapshot_type", "none,events_snapshot,tasks_snapshot,issues_snapshot,reports_snapshot")
+                .Annotation("Npgsql:Enum:task_current_status", "none,to_do,in_progress,review,done")
+                .Annotation("Npgsql:Enum:task_type", "none,abstract_goal,meeting_presense,job_complete")
+                .Annotation("Npgsql:Enum:user_role", "none,user,admin")
+                .OldAnnotation("Npgsql:Enum:decision_type", "none,default,apply,deny")
+                .OldAnnotation("Npgsql:Enum:event_status", "none,not_started,within_reminder_offset,live,finished,cancelled")
+                .OldAnnotation("Npgsql:Enum:event_type", "none,personal,one_to_one,stand_up,meeting")
+                .OldAnnotation("Npgsql:Enum:group_type", "none,educational,job")
+                .OldAnnotation("Npgsql:Enum:issue_type", "none,bag_issue,violation_issue")
+                .OldAnnotation("Npgsql:Enum:snapshot_type", "none,events_snapshot,tasks_snapshot,issues_snapshot,reports_snapshot")
+                .OldAnnotation("Npgsql:Enum:task_current_status", "none,to_do,in_progress,review,done")
+                .OldAnnotation("Npgsql:Enum:task_type", "none,abstract_goal,meeting_presense,job_complete")
+                .OldAnnotation("Npgsql:Enum:user_role", "none,user,admin");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "create_moment",
+                table: "snapshots",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<IssueStatus>(
+                name: "status",
+                table: "issues",
+                type: "issue_status",
+                nullable: false,
+                defaultValue: IssueStatus.None);
+
+            migrationBuilder.AddColumn<int>(
+                name: "manager_id",
+                table: "groups",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_groups_manager_id",
+                table: "groups",
+                column: "manager_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_groups_users_manager_id",
+                table: "groups",
+                column: "manager_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_groups_users_manager_id",
+                table: "groups");
+
+            migrationBuilder.DropIndex(
+                name: "ix_groups_manager_id",
+                table: "groups");
+
+            migrationBuilder.DropColumn(
+                name: "create_moment",
+                table: "snapshots");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "issues");
+
+            migrationBuilder.DropColumn(
+                name: "manager_id",
+                table: "groups");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:decision_type", "none,default,apply,deny")
+                .Annotation("Npgsql:Enum:event_status", "none,not_started,within_reminder_offset,live,finished,cancelled")
+                .Annotation("Npgsql:Enum:event_type", "none,personal,one_to_one,stand_up,meeting")
+                .Annotation("Npgsql:Enum:group_type", "none,educational,job")
+                .Annotation("Npgsql:Enum:issue_type", "none,bag_issue,violation_issue")
+                .Annotation("Npgsql:Enum:snapshot_type", "none,events_snapshot,tasks_snapshot,issues_snapshot,reports_snapshot")
+                .Annotation("Npgsql:Enum:task_current_status", "none,to_do,in_progress,review,done")
+                .Annotation("Npgsql:Enum:task_type", "none,abstract_goal,meeting_presense,job_complete")
+                .Annotation("Npgsql:Enum:user_role", "none,user,admin")
+                .OldAnnotation("Npgsql:Enum:decision_type", "none,default,apply,deny")
+                .OldAnnotation("Npgsql:Enum:event_status", "none,not_started,within_reminder_offset,live,finished,cancelled")
+                .OldAnnotation("Npgsql:Enum:event_type", "none,personal,one_to_one,stand_up,meeting")
+                .OldAnnotation("Npgsql:Enum:group_type", "none,educational,job")
+                .OldAnnotation("Npgsql:Enum:issue_status", "none,reported,in_progress,closed")
+                .OldAnnotation("Npgsql:Enum:issue_type", "none,bag_issue,violation_issue")
+                .OldAnnotation("Npgsql:Enum:snapshot_type", "none,events_snapshot,tasks_snapshot,issues_snapshot,reports_snapshot")
+                .OldAnnotation("Npgsql:Enum:task_current_status", "none,to_do,in_progress,review,done")
+                .OldAnnotation("Npgsql:Enum:task_type", "none,abstract_goal,meeting_presense,job_complete")
+                .OldAnnotation("Npgsql:Enum:user_role", "none,user,admin");
+        }
+    }
+}
