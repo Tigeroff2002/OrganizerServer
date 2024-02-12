20f6709ed2b873522cc370d38cae2811805e9499
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-02-12 11:41:34 +0300

    add mapping for context

--- PostgreSQL/CalendarDataContext.cs
+++ PostgreSQL/CalendarDataContext.cs
@@ -44,7 +44,8 @@ public class CalendarDataContext : DbContext
             .MapEnum<SnapshotType>()
             .MapEnum<DecisionType>()
             .MapEnum<IssueType>()
-            .MapEnum<UserRole>();
+            .MapEnum<UserRole>()
+            .MapEnum<IssueStatus>();
 
     public CalendarDataContext(DbContextOptions<CalendarDataContext> options)
         : base(options)
@@ -69,7 +70,8 @@ public class CalendarDataContext : DbContext
             .HasPostgresEnum<SnapshotType>()
             .HasPostgresEnum<DecisionType>()
             .HasPostgresEnum<IssueType>()
-            .HasPostgresEnum<UserRole>();
+            .HasPostgresEnum<UserRole>()
+            .HasPostgresEnum<IssueStatus>();
 
         CreateUsersModels(modelBuilder);
         CreateUserDevicesMap(modelBuilder);
@@ -238,6 +240,9 @@ public class CalendarDataContext : DbContext
             .Property(x => x.SnapshotType);
 
         _ = modelBuilder.Entity<Snapshot>()
+            .Property(x => x.CreateMoment);
+
+        _ = modelBuilder.Entity<Snapshot>()
             .Property(x => x.BeginMoment);
 
         _ = modelBuilder.Entity<Snapshot>()
@@ -272,6 +277,9 @@ public class CalendarDataContext : DbContext
 
         _ = modelBuilder.Entity<Issue>()
             .Property(x => x.IssueType);
+
+        _ = modelBuilder.Entity<Issue>()
+            .Property(x => x.Status);
 
         _ = modelBuilder.Entity<Issue>()
             .Property(x => x.Description);
